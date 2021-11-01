1743978e909c86ba5ff5144f90bd32099a21f2f2
filipesanches <53585862+filipesanches@users.noreply.github.com>
2021-11-01 20:20:28 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,12 @@
+
+function menu() {
+  if (document.getElementById("menu").style.display === "none") {
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("mudaico").classList.remove("fa-bars");
+    document.getElementById("mudaico").classList.add("fa-times");
+}else{
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("mudaico").classList.remove("fa-times");
+  document.getElementById("mudaico").classList.add("fa-bars");
+}
+}
